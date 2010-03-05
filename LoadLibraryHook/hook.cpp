44d75cfde21dd11d6310783d80840dc4dcb5c5d7
AnthIste <anthiste.anthiste@gmail.com>
2010-03-05 20:00:58 +0200

    Fixed small errors and still working on hook functions

--- LoadLibraryHook/hook.cpp
+++ LoadLibraryHook/hook.cpp
@@ -1,6 +1,11 @@
 #include "hook.h"
 
-unsigned long GetVtableAddress(void* pObject)
+unsigned long* GetVtableAddress(void* pObject)
 {
-	return *static_cast<unsigned long*>(object);
+	return static_cast<unsigned long*>(*static_cast<unsigned long*>(pObject));
+}
+
+unsigned long* DetourWithVtable(unsigned long* vtableAddress, unsigned int offset, unsigned long* hookProc)
+{
+	
 }
