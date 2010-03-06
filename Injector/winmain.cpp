1b8cd507e6cb1eecb856d2b1e58b493785293b85
AnthIste <anthiste.anthiste@gmail.com>
2010-03-06 09:59:45 +0200

    Added typedefs for maps and lists and more function shells

--- Injector/winmain.cpp
+++ Injector/winmain.cpp
@@ -1,6 +1,10 @@
 #include <windows.h>
+#include "injector.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	CInjector injector;
+	CInjector::ProcessList_t processes = injector.GetProcessList();
+
 	return 0;
 }
