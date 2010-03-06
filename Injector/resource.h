42b0068784cddc8142d4c76120ff2f435a305058
AnthIste <anthiste.anthiste@gmail.com>
2010-03-06 13:07:39 +0200

    Oops. Actually added the resource files to the repo xD

---
+++ Injector/resource.h
@@ -0,0 +1,11 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             100
+#define IDC_BTN_INJECT                          1000
+#define IDC_BTN_UNLOAD                          1008
+#define IDC_BTN_REFRESH                         1009
+#define IDC_BTN_DLL                             1010
+#define IDC_EDIT_DLL                            1011
+#define IDC_LIST_PROCESSES                      1012
