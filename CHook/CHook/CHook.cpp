0be3c9833ee58b4f39faee1ea1434ef514db27af
illuz1oN <ljmx90@live.co.uk>
2010-03-06 16:10:00 +0000

    Added CHook library, 0% completed as of now.

---
+++ CHook/CHook/CHook.cpp
@@ -0,0 +1,13 @@
+#define _WINNT_VER 0x0501
+#include <windows.h>
+#include "CHook.h"
+
+CHook::CHook( void )
+{
+	
+}
+
+CHook::~CHook( void )
+{
+
+}
---
+++ CHook/CHook/CHook.h
@@ -0,0 +1,10 @@
+#ifndef __CHOOK_H
+#define __CHOOK_H
+
+class CHook {
+	public:
+		CHook( void );
+		~CHook( void );
+};
+
+#endif
